cb281eaad81f64df9426a4c2b6a6d53dc549e770
qiucheng <qiucheng@yy.com>
2019-05-21 17:10:20 +0800

    实现项目框架按需加载安装，减少脚手架包体积

--- commands/create.js
+++ commands/create.js
@@ -2,6 +2,7 @@ const logger = require('../lib/util/logger')
 const fs = require('fs-extra')
 const path = require('path')
 const rimraf = require('rimraf')
+const { spawn } = require('child_process')
 const ora = require('ora')
 const { copyTpl } = require('../lib/util')
 process.env._CWD = process.cwd()
@@ -17,24 +18,45 @@ let create = {
     },
     generator: function(config) {
         let { name, library, tool } = config
-        if(!fs.existsSync(path.resolve(__dirname, `../node_modules/@jermken/qc-${library}-${tool}-seed/template`))) {
+        if(!require('../config/tpl.json')[`qc-${library}-${tool}-seed`]) {
             return logger.error(`template <${library}-${tool}> is not yet supported`)
         }
         if (fs.existsSync(path.join(process.env._CWD, `/${name}`))) {
             rimraf.sync(path.join(process.env._CWD, `/${name}`))
         }
+        if(require('../package.json').dependencies[`@jermken/qc-${library}-${tool}-seed`]) {
+            this.mkdir(name, library, tool)
+        } else {
+            let dir = path.resolve(__dirname, '../')
+            let spinner = ora('project template loading... \n')
+            spinner.start()
+            let _spawn = spawn(`${dir.substr(0,2)} && cd ${dir} && npm install @jermken/qc-${library}-${tool}-seed@latest --save`, {shell: true})
+            _spawn.stdout.on('data', (data) => {
+                logger.log(data.toString())
+            })
+            _spawn.on('close', (code) => {
+                if(code === 0) {
+                    spinner.succeed()
+                    this.mkdir(name, library, tool)
+                } else {
+                    spinner.fail()
+                }
+            })
+        }
+    },
+    mkdir: function(name, library, tool) {
         fs.mkdir(path.join(process.env._CWD, `/${name}`), (err) => {
             if (err) {
                 logger.error(err)
             } else {
-                let spinner = ora('project generating...')
-                spinner.start()
-                copyTpl(path.resolve(__dirname, '../node_modules/@jermken/qc-vue-webpack-seed/template'), path.join(process.env._CWD, `/${name}`)).then(() => {
-                    spinner.succeed()
+                let _spinner = ora('project generating...')
+                _spinner.start()
+                copyTpl(path.resolve(__dirname, `../node_modules/@jermken/qc-${library}-${tool}-seed/template`), path.join(process.env._CWD, `/${name}`)).then(() => {
+                    _spinner.succeed()
                     logger.success(`project generated successfully, please cd ${name}`)
                     this.qcConfigInit({title: name})
                 }).catch(err => {
-                    spinner.fail()
+                    _spinner.fail()
                     logger.error(err)
                 })
             }
--- commands/dev.js
+++ commands/dev.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const fs = require('fs')
+const { spawn } = require('child_process')
+const ora = require('ora')
 const logger = require('../lib/util/logger')
 process.env.NODE_ENV = 'development'
 process.env._CWD = process.cwd()
@@ -20,11 +22,29 @@ class DevCompiler {
 module.exports = (entry, cmd) => {
     let config;
     let configUrl = path.resolve(process.env._CWD, './qc.config.json')
+    let qcPackage = require('../package.json')
     if(fs.existsSync(configUrl)) {
         config = require(configUrl)
     } else {
         return logger.error(`file:${configUrl} is not found`)
     }
-    let compiler = new DevCompiler(config)
-    compiler.run()
+    if(!qcPackage.dependencies[`@jermken/qc-${config.lib}-${config.packer}-seed`]) {
+        let dir = path.resolve(__dirname, '../')
+        let spinner = ora('qc-cli is updating... \n')
+        spinner.start()
+        let _spawn = spawn(`${dir.substr(0,2)} && cd ${dir} && npm install @jermken/qc-${config.lib}-${config.packer}-seed@latest --save`, {shell: true})
+        _spawn.stdout.on('data', (data) => {
+            logger.log(data.toString())
+        })
+        _spawn.on('close', (code) => {
+            if(code === 0) {
+                spinner.succeed()
+                new DevCompiler(config).run()
+            } else {
+                spinner.fail()
+            }
+        })
+    } else {
+        new DevCompiler(config).run()
+    }
 }
